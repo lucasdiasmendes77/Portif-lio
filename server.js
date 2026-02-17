a367fffe6b51ec1c46fea1c8edb40b7268739dc6
lucasdiasmendes77 <lucasdiasmendes77@gmail.com>
2026-02-17 18:11:48 -0300

    Corrigindo estrutura head e SEO

--- server.js
+++ server.js
@@ -21,3 +21,10 @@ app.post("/contato", (req, res) => {
 app.listen(3000, () => {
     console.log("Servidor rodando em http://localhost:3000");
 });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
